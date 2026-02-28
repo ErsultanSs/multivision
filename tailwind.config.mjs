4d18a411b279e2f3f3f4b36ddce3880ff96ccf36
YersultanSS <ersultan.sabyrkhanov@gmail.com>
2026-02-28 12:02:05 +0500

    feat: Introduce new responsive breakpoints and optimize component layouts and typography for larger screens.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,6 +9,15 @@ export default {
 			boxShadow: {
 				"header-button": "0px 0px 33px 0px rgba(255, 255, 255, 0.2)",
 			},
+			screens: {
+				"3xl": "1920px",
+				"4xl": "2560px",
+			},
+			maxWidth: {
+				"8xl": "1440px",
+				"9xl": "1600px",
+				"10xl": "1800px",
+			},
 		},
 	},
 	plugins: [],
